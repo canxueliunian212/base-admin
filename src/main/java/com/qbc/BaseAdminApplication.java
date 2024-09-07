1cc7783a3dc00ea14bb5c12f4317e1dab86833d7
canxueliunian212 <canxueliunian212@163.com>
2024-09-07 17:47:16 +0900

    Initial commit

---
+++ src/main/java/com/qbc/BaseAdminApplication.java
@@ -0,0 +1,18 @@
+package com.qbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassNme BaseAdminApplication
+ * @Description TODO
+ * @Author chenpei
+ * @Date 2024/09/07 17:27
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class BaseAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BaseAdminApplication.class, args);
+    }
+}
